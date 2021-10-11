3d3dac72355c2b11a1355bf58d1d9cb34b4d14fc
tonilivaja <livajatenja@gmail.com>
2021-10-11 16:42:42 +0200

    Updated __init__ for authentication

--- website/__init__.py
+++ website/__init__.py
@@ -13,12 +13,22 @@ def create_app():
 	db.init_app(app)
 
 	from .views import views
+	from .auth import auth
 
 	app.register_blueprint(views, url_prefix='/')
+	app.register_blueprint(auth, url_prefix='/')
 
 	from .models import User, TestPC, Platform
 
 	create_database(app)
+
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+
+	@login_manager.user_loader
+	def load_user(id):
+		return User.query.get(int(id))
 
 	return app
 
